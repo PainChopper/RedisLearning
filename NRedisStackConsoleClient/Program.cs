8ec4b534d1fe26813d3e6dc9b46c43c5e1888f48
Виталий Сапановский <sapanovskiy_vn@astralnalog.ru>
2024-11-18 20:32:05 +0300

    Config and Save added

--- NRedisStackConsoleClient/Program.cs
+++ NRedisStackConsoleClient/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+using NRedisStack.RedisStackCommands;
+
+// Connect to Redis
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+
+var connectionString = configuration.GetConnectionString("Redis");
+var redis = await ConnectionMultiplexer.ConnectAsync(connectionString);
+var db = redis.GetDatabase();
+
+// Access stack commands using NRedisStack
+var jsonCommands = db.JSON();
+
+// Example of using JSON.SET command to save an object
+var jsonData = new { Name = "Vitaliy", Age = 52, City = "Sochi" };
+await jsonCommands.SetAsync("user:NumberTwo", "$", jsonData);
+
+// Example of using JSON.GET command to retrieve an object
+var result = await jsonCommands.GetAsync("user:NumberTwo");
+Console.WriteLine($"Retrieved data: {result}");
+
+// Save changes before closing
+await db.ExecuteAsync("SAVE");
+
+// Close the connection
+await redis.CloseAsync();
